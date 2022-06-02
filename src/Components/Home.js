e1988a67c10c2ad47d002d2b33c3d73cd5e1a5b2
Sahil <sahil.saxena75@gmail.com>
2022-06-02 20:13:20 +0530

    feat:added signin info

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,6 +6,10 @@ const Home = (props) => {
 
     const userLoggedIn = useSelector(state=>state.user.isAuthenticated)
 
+    const handleCopy = (e) => {
+        navigator.clipboard.writeText(e.target.value)
+    }
+
     return (
         <div>
             {userLoggedIn?
@@ -13,6 +17,12 @@ const Home = (props) => {
             :
             <div>
                 <h3>Please Login</h3>
+                    <div style={{textAlign:'center', marginTop:'32px', background:'#F5F5F5', padding:'16px', borderRadius:'12px'}}>
+                    <h4>Credentials</h4>
+                    <p><b>Email</b>: sahil@dctacademy.com </p>
+                    <button style={{marginBottom:'24px'}} class="btn btn-outline-primary" value="dct6543@gmail.com" onClick={handleCopy}>Copy</button>
+                    <p><b>Password</b>: secret123</p>
+                    </div>
             </div>
         }
         </div>
